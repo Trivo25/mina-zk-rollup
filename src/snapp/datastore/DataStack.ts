1fc13fbd6d51c0ec1af43b6e6aa1ccb2e240c539
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2022-02-10 13:25:35 +0100

    start work on DataStack

--- src/snapp/datastore/DataStack.ts
+++ src/snapp/datastore/DataStack.ts
@@ -1,1 +1,35 @@
-class DataStack<V> {}
+import { CircuitValue, Poseidon } from 'snarkyjs';
+import { MerkleTree } from './MerkleTree';
+
+class DataStore<V extends CircuitValue> {
+  dataStore: Array<V>;
+  merkleTree: MerkleTree;
+
+  constructor() {
+    this.dataStore = new Array<V>();
+    this.merkleTree = new MerkleTree();
+  }
+
+  pop(): V | undefined {
+    let leaves = this.merkleTree.tree.leaves;
+    let poppedElement = this.dataStore.pop();
+    leaves.pop();
+    this.merkleTree.clear();
+    this.merkleTree.addLeaves(leaves);
+    return poppedElement;
+  }
+
+  push(value: V): number {
+    this.merkleTree.addLeaves(value.toFields(), true);
+
+    return this.dataStore.push(value);
+  }
+
+  shift() {}
+
+  unshift() {}
+
+  splice() {}
+
+  getProof() {}
+}
