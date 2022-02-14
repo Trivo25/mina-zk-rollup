f091c50230cd8fdcf534e0f097998ec52bb8e4c9
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2022-02-14 23:41:48 +0100

    jest to mocha

---
+++ src/ex.ts
@@ -0,0 +1,8 @@
+import { Field } from 'snarkyjs';
+
+export { test };
+
+function test(): Field {
+  let f = Field(0);
+  return f;
+}
---
+++ tests/test.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+import { Field } from 'snarkyjs';
+import { test } from '../src/ex';
+
+it('pass', () => {
+  expect(test().equals(Field(0)).toBoolean());
+});
