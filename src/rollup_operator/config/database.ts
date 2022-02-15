4d37182b24e6a84122ad6ae1feff07723b16f774
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2022-02-15 13:17:36 +0100

    add database singelton

---
+++ src/rollup_operator/config/database.ts
@@ -0,0 +1,30 @@
+import pkg from 'pg';
+
+class SingletonPool extends pkg.Pool {
+  constructor() {
+    super({
+      user: process.env.PSQL_user,
+      database: process.env.PSQL_database,
+      password: process.env.PSQL_password,
+      port: parseInt(process.env.PSQL_port!),
+      host: process.env.PSQL_host,
+    });
+  }
+}
+
+class Connection {
+  static instance: SingletonPool | undefined;
+
+  constructor() {
+    throw new Error('Use Connection.getInstance() instead!');
+  }
+
+  static getInstance() {
+    if (Connection.instance === undefined) {
+      Connection.instance = new SingletonPool();
+    }
+    return Connection.instance;
+  }
+}
+
+export default Connection;
--- src/rollup_operator/index.ts
+++ src/rollup_operator/index.ts
@@ -1,6 +1,17 @@
+import 'dotenv/config';
+
+import Connection from './config/database.js';
 import server from './config/server.js';
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
-});
+
+init();
+
+async function init() {
+  let client = await Connection.getInstance().connect();
+  let res = await client.query('SELECT * FROM persons');
+  console.log(res.rows);
+  server.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`);
+  });
+}
