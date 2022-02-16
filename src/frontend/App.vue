eedbfca22b29e8e9961d58d2592123236e629e22
flkl192b <Florian.kluge@mailbox.tu-dresden.de>
2022-02-16 10:48:53 +0100

    re init frontend

--- src/frontend/App.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <h1 class="text-3xl font-bold underline text-center">Hello world!</h1>
-</template>
--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './index.css';
+import 'virtual:windi.css';
+
 createApp(App).mount('#app');
