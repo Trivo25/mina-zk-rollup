0243a86edfb00075b322c7df9c2129e6ac29f442
Florian <trivoplays@gmail.com>
2022-03-28 16:11:06 +0200

    add footer

--- src/frontend/src/components/SideNav.vue
+++ src/frontend/src/components/SideNav.vue
@@ -1,23 +1,30 @@
 <template>
   <nav class="bg dark:bg-dark">
     <ul>
-      <li>
+      <li class="selected">
         <div class="logo"><overview class="i" /></div>
         <span class="nav-item">Overview</span>
       </li>
-      <li>
+      <li class="selecteable">
         <div class="logo"><tx class="i" /></div>
         <span class="nav-item">Transactions</span>
       </li>
-      <li>
+      <li class="selecteable">
         <div class="logo"><block class="i" /></div>
         <span class="nav-item">Blocks</span>
       </li>
-      <li>
+      <li class="selecteable">
         <div class="logo"><accounts class="i" /></div>
         <span class="nav-item">Accounts</span>
       </li>
     </ul>
+
+    <div class="footer">
+      <h1>zk-Rollup</h1>
+      <a href="https://github.com/Trivo25/mina-zk-rollup" target="_blank"
+        ><github />GitHub</a
+      >
+    </div>
   </nav>
 </template>
 
@@ -26,26 +33,30 @@ import block from '~icons/carbon/Blockchain';
 import tx from '~icons/carbon/DataShare';
 import overview from '~icons/carbon/Dashboard';
 import accounts from '~icons/carbon/Events';
+import github from '~icons/carbon/LogoGithub';
 </script>
 
 <style scoped>
 .bg {
   height: 100vh;
   width: 250px;
-  position: sticky;
   z-index: 1;
   top: 25px;
   left: 0;
   overflow-x: hidden;
-  padding-top: 60px;
+  padding-top: 30px;
   transition: 0.2s;
-  background-color: var(--nord9) !important;
-  box-shadow: 0px 4px 4px black;
+  position: sticky;
+  border-right: 1px solid black;
+
+  /*   background-color: var(--nord9); */
+  /*   box-shadow: 0px 4px 4px black; */
 }
 
 .dark .dark\:bg-dark {
-  background-color: var(--nord1) !important;
-  box-shadow: 0px 4px 4px rgb(0, 0, 0);
+  /*   background-color: var(--nord1);
+ */
+  /*   box-shadow: 0px 4px 4px rgb(0, 0, 0); */
 }
 
 ul {
@@ -63,11 +74,16 @@ li {
   border-width: 1px;
   border-color: black;
   cursor: pointer;
-  border-right: 6px solid transparent;
   transition: 0.5s;
+  border-right: 1px solid var(--nord1);
 }
 
-li:hover {
+.selecteable:hover {
+  transition: 0.5s;
+  border-right: 6px solid var(--nord13);
+}
+
+.selected {
   border-right: 6px solid var(--nord13);
 }
 
@@ -92,5 +108,13 @@ li:hover {
   padding-left: 10px;
   font-size: 1.3rem;
   color: var(--nord2);
+}
+
+.footer {
+  position: absolute;
+  bottom: 100px;
+  width: 100%;
+  color: var(--nord3);
+  line-height: 30px;
 }
 </style>
