aebbc3673656fef1dc19ee08122a5e09e99bba85
Florian <trivoplays@gmail.com>
2022-03-28 09:55:27 +0200

    add logo

--- src/frontend/src/components/Header.vue
+++ src/frontend/src/components/Header.vue
@@ -14,12 +14,12 @@
           <div class="flex-shrink-0 flex items-center">
             <img
               class="block lg:hidden h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+              src="../assets/logo.svg"
               alt="rollup"
             />
             <img
               class="hidden lg:block h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+              src="../assets/logo.svg"
               alt="rollup"
             />
           </div>
