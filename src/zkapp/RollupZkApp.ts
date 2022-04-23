57c88701660c6fb8db60a39b71fd2ba033b30e79
Florian <florian@technotro.com>
2022-04-23 16:26:38 +0200

    snapp -> zkapp, refac

--- src/zkapp/RollupZkApp.ts
+++ src/zkapp/RollupZkApp.ts
@@ -2,28 +2,22 @@ import {
   Bool,
   Circuit,
   Field,
-  isReady,
   method,
-  Mina,
-  Party,
   Poseidon,
-  PrivateKey,
   PublicKey,
-  shutdown,
   Signature,
   SmartContract,
   State,
   state,
   UInt64,
 } from 'snarkyjs';
-import { MerkleStack } from '../lib/data_store/MerkleStack';
 
-import { MerkleTree } from '../lib/merkle_proof/MerkleTree';
+import { MerkleStack } from '../lib/data_store/MerkleStack';
 import RollupAccount from '../rollup_operator/rollup/models/RollupAccount';
 import RollupDeposit from '../rollup_operator/rollup/models/RollupDeposit';
 import RollupProof from '../rollup_operator/rollup/RollupProof';
 
-class RollupSnapp extends SmartContract {
+export default class RollupZkApp extends SmartContract {
   // Merkle root of all accounts
   @state(Field) acccountsCommitment = State<Field>();
   // Merkle root of all pending deposits
@@ -118,76 +112,4 @@ class RollupSnapp extends SmartContract {
 
     return proofHash.equals(currentAcccountsCommitment);
   }
-}
-
-test();
-
-async function test() {
-  await isReady;
-
-  const Local = Mina.LocalBlockchain();
-  Mina.setActiveInstance(Local);
-
-  const account1 = Local.testAccounts[0].privateKey;
-  const account2 = Local.testAccounts[1].privateKey;
-
-  const snappPrivKey = PrivateKey.random();
-  const snappPubKey = snappPrivKey.toPublicKey();
-
-  // creating a new merkle'ized off chain storage
-
-  // let leaves: Field[] = [Field(0), Field(1), Field(2), Field(3), Field(4)];
-
-  // having some more data
-  let leaves: Field[] = [];
-  for (let i = 0; i < 100; i++) {
-    leaves.push(Field(Math.floor(Math.random() * 1000)));
-  }
-
-  let merkleTree: MerkleTree = new MerkleTree();
-  merkleTree.addLeaves(leaves);
-
-  await Mina.transaction(account1, async () => {
-    let snapp = new RollupSnapp(snappPubKey);
-
-    const amount = UInt64.fromNumber(1000000);
-    const p = await Party.createSigned(account2);
-    p.balance.subInPlace(amount);
-    let previousRoot = merkleTree.getMerkleRoot();
-
-    if (previousRoot === undefined) {
-      console.log('Merkel root undefined');
-      previousRoot = Field(0);
-    }
-
-    snapp.deploy(amount, previousRoot, Field(0));
-  })
-    .send()
-    .wait();
-
-  // let snappState = (await Mina.getAccount(snappPubKey)).snapp.appState[0];
-  // console.log('initial state: ' + snappState);
-
-  await Mina.transaction(account1, async () => {
-    let snapp = new RollupSnapp(snappPubKey);
-
-    // make the contract verify every leaf inside the original leaves array
-    leaves.forEach(async (leaf, i) => {
-      let res = await snapp.validateProof(
-        merkleTree.getProof(i),
-        Poseidon.hash([leaf])
-      );
-
-      Circuit.asProver(() => {
-        console.log(`existance of ${leaf.toString()}`, res.toBoolean());
-      });
-    });
-  })
-    .send()
-    .wait();
-
-  // snappState = (await Mina.getAccount(snappPubKey)).snapp.appState[0];
-  // console.log('final state: ' + snappState);
-
-  shutdown();
 }
