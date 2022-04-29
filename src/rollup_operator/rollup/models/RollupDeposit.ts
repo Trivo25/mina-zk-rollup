a58fd82c9a21f34b5524b59bdc3315096fee2db3
Florian <florian@technotro.com>
2022-04-29 09:48:29 +0200

    Update RollupDeposit.ts

--- src/rollup_operator/rollup/models/RollupDeposit.ts
+++ src/rollup_operator/rollup/models/RollupDeposit.ts
@@ -1,11 +1,16 @@
 import { CircuitValue, Field, prop, PublicKey, UInt64 } from 'snarkyjs';
 
+/**
+ * A {@link RollupDeposit} descibes the action when a user wants to deposit funds to the layer 2.
+ */
 export default class RollupDeposit extends CircuitValue {
   @prop publicKey: PublicKey;
   @prop amount: UInt64;
-  constructor(publicKey: PublicKey, amount: UInt64) {
+  @prop tokenId: UInt64;
+  constructor(publicKey: PublicKey, amount: UInt64, tokenId: UInt64) {
     super();
     this.publicKey = publicKey;
     this.amount = amount;
+    this.tokenId = tokenId;
   }
 }
