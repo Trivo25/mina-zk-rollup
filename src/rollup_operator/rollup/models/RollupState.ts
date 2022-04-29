961aeffcf32b237fbc24e676ecd788bbcc95409e
Florian <florian@technotro.com>
2022-04-29 09:49:37 +0200

    Update RollupState.ts

--- src/rollup_operator/rollup/models/RollupState.ts
+++ src/rollup_operator/rollup/models/RollupState.ts
@@ -1,5 +1,10 @@
 import { CircuitValue, Field, prop } from 'snarkyjs';
 
+/**
+ * The {@link RollupState} descibes the current state of the layer 2.
+ * It currently includes the merkle root of all accounts that exist on the layer 2
+ * and the merkle root of all pending deposits.
+ */
 export default class RollupState extends CircuitValue {
   @prop pendingDepositsCommitment: Field;
   @prop accountDbCommitment: Field;
