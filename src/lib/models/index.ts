adedce0113baafc2fd5a4599148d539998972920
Florian <florian@technotro.com>
2022-04-29 10:08:55 +0200

    update interfaces

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -6,6 +6,16 @@ import IHTTPResponse from './interfaces/IHTTPResponse';
 import IPublicKey from './interfaces/IPublicKey';
 import ISignature from './interfaces/ISignature';
 import ITransaction from './interfaces/ITransaction';
+import ISerializable from './interfaces/ISerializable';
+import IDeserializable from './interfaces/IDeserializable';
 
 export { EnumError, Events };
-export type { IBlock, IHTTPResponse, IPublicKey, ISignature, ITransaction };
+export type {
+  IBlock,
+  IHTTPResponse,
+  IPublicKey,
+  ISignature,
+  ITransaction,
+  IDeserializable,
+  ISerializable,
+};
