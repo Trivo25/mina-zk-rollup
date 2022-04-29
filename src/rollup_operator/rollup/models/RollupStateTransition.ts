c726af0872a4327ea917e46a17a58d4f44aff583
Florian <florian@technotro.com>
2022-04-29 09:51:14 +0200

    Update RollupStateTransition.ts

--- src/rollup_operator/rollup/models/RollupStateTransition.ts
+++ src/rollup_operator/rollup/models/RollupStateTransition.ts
@@ -2,6 +2,11 @@ import { CircuitValue, Field, prop } from 'snarkyjs';
 
 import RollupState from './RollupState';
 
+/**
+ * A {@link RollupStateTransition} descibes the transition that takes place when
+ * the rollup operator updates the current state of the the layer 2 by providing a series of
+ * proofs that attest correct execution of transactions.
+ */
 export default class RollupStateTransition extends CircuitValue {
   @prop source: RollupState;
   @prop target: RollupState;
