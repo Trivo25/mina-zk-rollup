698594e5a957a17fc0ce4d061774acb784babe83
Florian <florian@technotro.com>
2022-06-15 23:39:34 +0200

    start groundworkl for leveldb store

--- src/rollup_operator/data_store/DataStore.ts
+++ src/rollup_operator/data_store/DataStore.ts
@@ -1,16 +1,4 @@
 class DataStore {
-  getTransactionPool(): any {
-    throw new Error('Method not implemented.');
-  }
-  getTransactionHistory() {
-    throw new Error('Method not implemented.');
-  }
-  getAccountStore() {
-    throw new Error('Method not implemented.');
-  }
-
-  store: any;
-
   constructor() {}
 }
 
--- src/rollup_operator/data_store/LevelStore.ts
+++ src/rollup_operator/data_store/LevelStore.ts
@@ -1,2 +1,34 @@
 import { DataStore } from '.';
-export default class LevelStore extends DataStore {}
+import { Level } from 'level';
+
+import {
+  AbstractBatchPutOperation,
+  AbstractBatchDelOperation,
+  AbstractSublevel,
+} from 'abstract-level';
+
+type Put = AbstractBatchPutOperation<Level<string, any>, string, any>;
+type Delete = AbstractBatchDelOperation<Level<string, any>, string>;
+
+type SubLevel = AbstractSublevel<
+  Level<string, any>,
+  string | Buffer | Uint8Array,
+  string,
+  string
+>;
+
+export default class LevelStore extends DataStore {
+  private store: Level<string, any>;
+  private nodes: SubLevel;
+  private leaves: SubLevel;
+
+  private instructionStack: (Put | Delete)[];
+
+  constructor(db: Level<string, any>, id: string) {
+    super();
+    this.instructionStack = [];
+    this.store = db;
+    this.nodes = this.store.sublevel(id);
+    this.leaves = this.store.sublevel(id + '-leaf');
+  }
+}
