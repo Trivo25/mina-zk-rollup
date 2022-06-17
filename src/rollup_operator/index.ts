2fae6016a1682666b86a3475977aaa19fed3baad
Florian <florian@technotro.com>
2022-06-17 18:05:52 +0200

    Update index.ts

--- src/rollup_operator/index.ts
+++ src/rollup_operator/index.ts
@@ -1,19 +1,16 @@
 import 'dotenv/config';
 import server from './setup/server.js';
-const PORT = process.env.PORT || 5000;
 
-let oldLog = console.log;
-console.log = function () {
-  var args = [].slice.call(arguments);
-  oldLog.apply(console.log, [`[${new Date().toISOString()}]`].concat(args));
-};
+import logger from '../lib/log';
+
+const PORT = process.env.PORT || 5000;
 
 init();
 
 async function init() {
-  console.log(`Starting operator..`);
+  logger.info(`Starting operator..`);
 
   server.listen(PORT, () => {
-    console.log(`Rollup operator running on port ${PORT}`);
+    logger.info(`Rollup operator running on port ${PORT}`);
   });
 }
