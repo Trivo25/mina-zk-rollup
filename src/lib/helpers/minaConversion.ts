bf61d3eb5802f4315ef6f49b29d2e2fd6ed4efa5
Florian <florian@technotro.com>
2022-07-28 13:29:26 +0200

    bigint support for nano conversion

--- src/lib/helpers/minaConversion.ts
+++ src/lib/helpers/minaConversion.ts
@@ -1,7 +1,7 @@
-export function nanoToMina(m: number): number {
-  return m * 10 ** 9;
+export function nanoToMina(m: number | bigint): bigint {
+  return BigInt(m) * BigInt(10) ** BigInt(-9);
 }
 
-export function minaToNano(m: number): number {
-  return m * 10 ** 9;
+export function minaToNano(m: number | bigint): bigint {
+  return BigInt(m) * BigInt(10) ** BigInt(9);
 }
