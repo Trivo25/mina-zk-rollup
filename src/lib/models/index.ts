20d1b7702f754c37f47206c2d3be65373291a410
Florian <florian@technotro.com>
2022-07-31 16:08:33 +0200

    add tx states enum

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -1,8 +1,9 @@
 import EnumError from './enums/EnumError';
+import EnumFinality from './enums/EnumFinality';
 
 import IBlock from './enums/interfaces/IBlock';
 import IHTTPResponse from './enums/interfaces/IHTTPResponse';
 import ITransaction from './enums/interfaces/ITransaction';
 
-export { EnumError };
+export { EnumError, EnumFinality };
 export type { IBlock, IHTTPResponse, ITransaction };
