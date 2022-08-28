6c98b9588f5f5b97838f67195ddfd0bd5d518d6e
Florian <florian@technotro.com>
2022-08-28 22:30:20 +0200

    setup manager

--- src/worker/client/manager.ts
+++ src/worker/client/manager.ts
@@ -1,1 +1,34 @@
-export {};
+import { CloudAPI, Instance } from './cloud_api/api';
+
+export class WorkerManager {
+  private c: CloudAPI;
+
+  private workerPool: WorkerPool | undefined = undefined;
+
+  constructor(c: CloudAPI) {
+    this.c = c;
+  }
+
+  prepareWorkerPool(load: any, options: PoolOptions): WorkerPool {
+    if (options.width % 2 != 0) {
+      throw new Error(
+        'Can not initialize worker pool with an odd amount of worker nodes.'
+      );
+    }
+    return {
+      pool: [],
+      start() {},
+    };
+  }
+
+  cleanUp() {}
+}
+
+interface PoolOptions {
+  width: number;
+}
+
+interface WorkerPool {
+  pool: Instance[];
+  start(): void;
+}
