3ef0ee6dbfc2ac6715d0f9e4b035e1b021d2d565
Florian <florian@technotro.com>
2022-08-28 21:31:07 +0200

    dummy run

--- src/worker/client/index.ts
+++ src/worker/client/index.ts
@@ -4,7 +4,20 @@ import {
   ProverServiceClient,
   IProverServiceClient,
 } from '../protos_gen/prover.grpc-client.js';
+import { AWS, Region } from './cloud_api/aws.js';
+import 'dotenv/config';
 
+let ec2 = new AWS(undefined, Region.US_EAST_1);
+
+/* let i = await ec2.createInstance();
+await ec2.stopInstances([i.id]);
+await ec2.startInstance([i.id]);
+await ec2.deleteInstance([i.id]);
+console.log(i);
+ */
+
+await ec2.listAll();
+/* 
 const client = new ProverServiceClient(
   'localhost:5000',
   ChannelCredentials.createInsecure(),
@@ -32,4 +45,4 @@ function echo(client: IProverServiceClient) {
   } catch (error) {
     console.log(error);
   }
-}
+} */
