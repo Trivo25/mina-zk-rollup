0890f28e946696d775d292f41fb77d060db6b426
Florian <florian@technotro.com>
2023-01-01 02:04:34 +0100

    acc abstraction

--- src/proof_system/account.ts
+++ src/proof_system/account.ts
@@ -7,10 +7,11 @@ import {
   Field,
   Poseidon,
   Bool,
+  Struct,
 } from 'snarkyjs';
 import { AccountWitness } from '../lib/data_store/AccountStore.js';
 import { base58Encode } from '../lib/helpers/base58.js';
-export { Account };
+export { Account, AuthRequired, Tuple };
 
 /**
  * An {@link Account} describes an account on the layer 2.
@@ -91,3 +92,46 @@ class Account extends CircuitValue {
     );
   }
 }
+
+type Tuple<T, N extends number> = N extends N
+  ? number extends N
+    ? T[]
+    : _TupleOf<T, N, []>
+  : never;
+type _TupleOf<T, N extends number, R extends unknown[]> = R['length'] extends N
+  ? R
+  : _TupleOf<T, N, [T, ...R]>;
+
+type AuthRequired = {
+  constant: Bool;
+  signatureNecessary: Bool;
+  signatureSufficient: Bool;
+};
+
+class Account_ extends Struct({
+  publicKey: PublicKey,
+  nonce: UInt32,
+  token: Field,
+  tokenSymbol: String,
+  zkappUri: String,
+  zkappState: {} as Tuple<Field | null, 8>,
+  receiptChainHash: Field,
+  balance: { total: UInt64 },
+  permissions: {
+    editState: {} as AuthRequired,
+    send: {} as AuthRequired,
+    receive: {} as AuthRequired,
+    setDelegate: {} as AuthRequired,
+    setPermissions: {} as AuthRequired,
+    setVerificationKey: {} as AuthRequired,
+    setZkappUri: {} as AuthRequired,
+    editSequenceState: {} as AuthRequired,
+    setTokenSymbol: {} as AuthRequired,
+    incrementNonce: {} as AuthRequired,
+    setVotingFor: {} as AuthRequired,
+  },
+  delegateAccount: { publicKey: PublicKey },
+  sequenceEvents: {} as Tuple<Field | null, 32>,
+  verificationKey: { data: Field, hash: Field },
+  provedState: Bool,
+}) {}
--- src/proof_system/transaction.ts
+++ src/proof_system/transaction.ts
@@ -11,12 +11,13 @@ import {
   Poseidon,
   PrivateKey,
   arrayProp,
+  Sign,
 } from 'snarkyjs';
 import { AccountWitness } from '../lib/data_store/AccountStore.js';
 import { DepositWitness } from '../lib/data_store/DepositStore.js';
 import { base58Encode } from '../lib/helpers/base58.js';
 import { EnumFinality, ITransaction, IDeposit } from '../lib/models/index.js';
-import { Account } from './account.js';
+import { Account, AuthRequired } from './account.js';
 
 export {
   AccountUpdate_,
@@ -290,3 +291,210 @@ class RollupDeposit extends CircuitValue {
     }
   }
 }
+
+type AuthorizationKind = { isSigned: Bool; isProved: Bool };
+type TokenId = Field;
+type TokenSymbol = { symbol: string; field: Field };
+type ZkappUri = { data: string; hash: Field };
+
+type AccountUpdate = {
+  body: {
+    publicKey: PublicKey;
+    tokenId: TokenId;
+    update: {
+      appState: { isSome: Bool; value: Field }[];
+      delegate: { isSome: Bool; value: PublicKey };
+      verificationKey: {
+        isSome: Bool;
+        value: {
+          data: string;
+          hash: Field;
+        };
+      };
+      permissions: {
+        isSome: Bool;
+        value: {
+          editState: AuthRequired;
+          send: AuthRequired;
+          receive: AuthRequired;
+          setDelegate: AuthRequired;
+          setPermissions: AuthRequired;
+          setVerificationKey: AuthRequired;
+          setZkappUri: AuthRequired;
+          editSequenceState: AuthRequired;
+          setTokenSymbol: AuthRequired;
+          incrementNonce: AuthRequired;
+          setVotingFor: AuthRequired;
+        };
+      };
+      zkappUri: {
+        isSome: Bool;
+        value: {
+          data: string;
+          hash: Field;
+        };
+      };
+      tokenSymbol: { isSome: Bool; value: TokenSymbol };
+      timing: {
+        isSome: Bool;
+        value: {
+          initialMinimumBalance: UInt64;
+          cliffTime: UInt32;
+          cliffAmount: UInt64;
+          vestingPeriod: UInt32;
+          vestingIncrement: UInt64;
+        };
+      };
+      votingFor: { isSome: Bool; value: Field };
+    };
+    balanceChange: {
+      magnitude: UInt64;
+      sgn: Sign;
+    };
+    incrementNonce: Bool;
+    events: {
+      data: Field[][];
+      hash: Field;
+    };
+    sequenceEvents: {
+      data: Field[][];
+      hash: Field;
+    };
+    callData: Field;
+    callDepth: number;
+    preconditions: {
+      network: {
+        snarkedLedgerHash: { isSome: Bool; value: Field };
+        timestamp: {
+          isSome: Bool;
+          value: {
+            lower: UInt64;
+            upper: UInt64;
+          };
+        };
+        blockchainLength: {
+          isSome: Bool;
+          value: {
+            lower: UInt32;
+            upper: UInt32;
+          };
+        };
+        minWindowDensity: {
+          isSome: Bool;
+          value: {
+            lower: UInt32;
+            upper: UInt32;
+          };
+        };
+        totalCurrency: {
+          isSome: Bool;
+          value: {
+            lower: UInt64;
+            upper: UInt64;
+          };
+        };
+        globalSlotSinceHardFork: {
+          isSome: Bool;
+          value: {
+            lower: UInt32;
+            upper: UInt32;
+          };
+        };
+        globalSlotSinceGenesis: {
+          isSome: Bool;
+          value: {
+            lower: UInt32;
+            upper: UInt32;
+          };
+        };
+        stakingEpochData: {
+          ledger: {
+            hash: { isSome: Bool; value: Field };
+            totalCurrency: {
+              isSome: Bool;
+              value: {
+                lower: UInt64;
+                upper: UInt64;
+              };
+            };
+          };
+          seed: { isSome: Bool; value: Field };
+          startCheckpoint: { isSome: Bool; value: Field };
+          lockCheckpoint: { isSome: Bool; value: Field };
+          epochLength: {
+            isSome: Bool;
+            value: {
+              lower: UInt32;
+              upper: UInt32;
+            };
+          };
+        };
+        nextEpochData: {
+          ledger: {
+            hash: { isSome: Bool; value: Field };
+            totalCurrency: {
+              isSome: Bool;
+              value: {
+                lower: UInt64;
+                upper: UInt64;
+              };
+            };
+          };
+          seed: { isSome: Bool; value: Field };
+          startCheckpoint: { isSome: Bool; value: Field };
+          lockCheckpoint: { isSome: Bool; value: Field };
+          epochLength: {
+            isSome: Bool;
+            value: {
+              lower: UInt32;
+              upper: UInt32;
+            };
+          };
+        };
+      };
+      account: {
+        balance: {
+          isSome: Bool;
+          value: {
+            lower: UInt64;
+            upper: UInt64;
+          };
+        };
+        nonce: {
+          isSome: Bool;
+          value: {
+            lower: UInt32;
+            upper: UInt32;
+          };
+        };
+        receiptChainHash: { isSome: Bool; value: Field };
+        delegate: { isSome: Bool; value: PublicKey };
+        state: { isSome: Bool; value: Field }[];
+        sequenceState: { isSome: Bool; value: Field };
+        provedState: { isSome: Bool; value: Bool };
+        isNew: { isSome: Bool; value: Bool };
+      };
+    };
+    useFullCommitment: Bool;
+    caller: TokenId;
+    authorizationKind: AuthorizationKind;
+  };
+  authorization: {
+    proof?: string;
+    signature?: string;
+  };
+};
+
+type ZkappCommand = {
+  feePayer: {
+    body: {
+      publicKey: PublicKey;
+      fee: UInt64;
+      validUntil?: UInt32;
+      nonce: UInt32;
+    };
+    authorization: string;
+  };
+  accountUpdates: AccountUpdate[];
+  memo: string;
+};
